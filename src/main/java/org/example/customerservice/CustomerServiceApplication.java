58397e609da71c2f75dfbfef307f8df1721195d3
Nissem01 <nils.modig02@gmail.com>
2026-08-20 10:30:13 +0200

    Added a Todo list in CustomerServiceApplication.

--- src/main/java/org/example/customerservice/CustomerServiceApplication.java
+++ src/main/java/org/example/customerservice/CustomerServiceApplication.java
@@ -10,4 +10,26 @@ public class CustomerServiceApplication {
         SpringApplication.run(CustomerServiceApplication.class, args);
     }
 
+
+
+/*    TODO UPPGIFT
+
+Byta namn på CustomerService repot/projektet
+
+Bryt ut CustomerService logiken från HotelBooking så långt som möjligt (fylla ut alla endpoints som behövs).
+Bygga om databasen på bokningssidan (ta bort customer tabellen).
+Kolla på att skriva bättre felmeddelande (t.ex, om kunden inte finns när man bokar. Just nu krashar sidan!).
+Se till att API anropen ska ha rätt status meddelande. (response entities?).
+Kolla på lösningar för ActiveCustomer problemet.
+2 Dockerfiler, en för varje app. 1 docker compose fil för allting sammansatt.
+Fundera på ytterligare microservice tjänst för vg (recensioner t.ex).
+
+Steg 8 – Felhantering mellan tjänsterna
+Stäng av kundtjänsten och försök skapa en bokning – vad händer?
+Tjänsten ska inte krascha! den ska ge ett tydligt felmeddelande
+
+Kolla på att ta bort Customer objekt/modellen från HotelBooking och istället bara utgå från customer ID utifrån booking tabellen.
+Kolla på hur lösenordet ska hanteras mellan HotelBooking och CustomerService, kan man skapa en separat request för att inte skicka lösen.
+
+*/
 }
