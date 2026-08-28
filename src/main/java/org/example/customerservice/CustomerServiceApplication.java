80d2efac56daeacbcffd0190f457c2152e09a9af
Filip Walter <filipwalter10@gmail.com>
2026-08-28 14:58:23 +0200

    edited todo

--- src/main/java/org/example/customerservice/CustomerServiceApplication.java
+++ src/main/java/org/example/customerservice/CustomerServiceApplication.java
@@ -6,32 +6,36 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CustomerServiceApplication {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
         SpringApplication.run(CustomerServiceApplication.class, args);
     }
 
-
+}
 
 /*    TODO UPPGIFT
 
 
 
 flytta allt från CustomerService På hotelsidan
+----Flytta bort hjälpfuntion från CustomerController i Hotelbooking
 
 Kolla på att skriva bättre felmeddelande (t.ex, om kunden inte finns när man bokar. Just nu krashar sidan!).
-Se till att API anropen ska ha rätt status meddelande. (response entities?).
-Kolla på lösningar för ActiveCustomer problemet.(linda?)
+--- funkar just nu på login.
 
+Se till att API anropen ska ha rätt status meddelande. (response entities?).
+-- finns just nu i login.
 
 Steg 8 – Felhantering mellan tjänsterna
 Stäng av kundtjänsten och försök skapa en bokning – vad händer?
 Tjänsten ska inte krascha! den ska ge ett tydligt felmeddelande
 
-Kolla på att ta bort Customer objekt/modellen från HotelBooking och istället bara utgå från customer ID utifrån booking tabellen.
-Kolla på hur lösenordet ska hanteras mellan HotelBooking och CustomerService, kan man skapa en separat request för att inte skicka lösen.
+Kolla över DTOS (delete det som inte behövs), se till så Customerservice inte returnerar lösenord till Hotellbooking
 
 2 Dockerfiler, en för varje app. 1 docker compose fil för allting sammansatt.
+
 Fundera på ytterligare microservice tjänst för vg (recensioner t.ex).
+--- eventuellt lägga in en knapp direkt i min sida?
+
 
 
 
@@ -39,4 +43,4 @@ FRÅGA OM GITHUB
 DTO
 
 */
-}
+
